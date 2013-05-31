8661e3fb4d577bfcadf04be5a36a1ffdbc5a4223
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-05-31 13:57:01 -0700

    Explore pure co-author matching

--- KDD/KDD/Script.fsx
+++ KDD/KDD/Script.fsx
@@ -4,6 +4,7 @@
 
 open System
 open System.IO
+open System.Text.RegularExpressions
 open KDD
 open KDD.Model
 
@@ -87,6 +88,40 @@ let smartDupes  = seq {
             |> individuals
         for author in data do
             yield (formatAuthor author) }
+
+let authorIds = authors |> Array.map (fun x -> x.AuthorId) |> Set.ofArray
+let candidates (authorId:int) =
+    let excluded = coAuthors authorId
+    seq { for excl in excluded do 
+              let coExcl = coAuthors excl
+              for candidate in coExcl do yield candidate }
+    |> Set.ofSeq
+    |> Set.filter (fun x -> authorIds.Contains(x))
+    |> Set.filter (fun x -> not (excluded.Contains(x)))
+
+authors.[0..99] |> Array.map (fun a -> candidates (a.AuthorId)) |> Array.iter (fun s -> printfn "%i candidates" (Set.count s))
+
+let test (authorIndex:int) =
+    let author = authors |> Array.find (fun x -> x.AuthorId = authorIndex)//authors.[authorIndex]
+    printfn "%s" author.Name
+    printfn ""
+    candidates (author.AuthorId) 
+    |> Seq.map (fun id -> 
+        let co = authors |> Array.find (fun x -> x.AuthorId = id) 
+        co.Name)
+    |> Seq.sort
+    |> Seq.iter (fun name -> printfn "%s" name)
+
+
+let options = RegexOptions.Compiled ||| RegexOptions.IgnoreCase
+let matchWords = new Regex(@"\w+", options)
+let vocabulary (text: string) =
+    matchWords.Matches(text)
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> m.Value)
+    |> Set.ofSeq
+
+let initials (chunks:Set<string>) = chunks |> Set.map (fun x -> x.Substring(0,1))
 
 let submitPath = @"C:\users\mathias\desktop\submit3.csv"
 let submit = File.WriteAllLines(submitPath, (smartDupes |> Seq.toArray))    
